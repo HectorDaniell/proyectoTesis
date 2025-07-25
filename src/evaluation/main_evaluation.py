b238e5757a021a8ac9fa8e82b4c1732a227b14e2
Hector Daniel Oviedo <91428248+HectorDaniell@users.noreply.github.com>
2025-07-25 11:26:02 -0500

    Comment language update main_evaluation.py

--- src/evaluation/main_evaluation.py
+++ src/evaluation/main_evaluation.py
@@ -2,15 +2,15 @@ from predict_performance import predict_performance
 import os
 base_path = os.path.abspath(os.path.join(os.path.dirname(__file__), '../../'))
 
-# Evaluar nuevos videos usando un modelo entrenado
+# Evaluate new videos using a trained model
 def main_evaluation():
-    video_file = os.path.join(base_path, f'data/raw/jump/jump_006.mp4')  # Video de entrada para evaluación
-    model_file = os.path.join(base_path, f'data/models/XGBoost_model.pkl')  # Ruta del modelo entrenado
-    pca_components = 10  # Número de componentes de PCA usados en el entrenamiento
+    video_file = os.path.join(base_path, f'data/raw/jump/jump_006.mp4')  # Input video for evaluation
+    model_file = os.path.join(base_path, f'data/models/XGBoost_model.pkl')  # Path to the trained model
+    pca_components = 10  # Number of PCA components used in training
     
-    # Hacer predicción
+    # Make prediction
     predict_performance(video_file, model_file, pca_components)
 
-# Ejecutar la evaluación del video
+# Run video evaluation
 if __name__ == "__main__":
     main_evaluation()
